9f76cdc08c81df62b0af7220b62d7c98e6720cc0
Kateryna Kleshch <ketrins1993@gmail.com>
2022-09-01 22:17:29 +0300

    add fetch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries.js'
 
 const DEBOUNCE_DELAY = 300;
+
+
+
+fetchCountries('cana')
+  .then(countries => console.log(countries))
+  .catch(error => console.log(error));
