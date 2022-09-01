96cea32b85795673bf8d8bfe72c4de2d312dea1d
Kateryna Kleshch <ketrins1993@gmail.com>
2022-09-02 00:41:51 +0300

    add render

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,12 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+const notifyOptions = {
+  timeout: 2500,
+  width: '300px',
+  clickToClose: true,
+  position: 'center-top',
+};
+
 const fields = ['name', 'capital', 'population', 'flags', 'languages'];
 
 export const fetchCountries = name => {
@@ -7,12 +14,7 @@ export const fetchCountries = name => {
     `https://restcountries.com/v3.1/name/${name}?fields=${fields.join(',')}`
   ).then(response => {
     if (!response.ok) {
-      Notify.failure('Oops, there is no country with that name', {
-        timeout: 2500,
-        width: '300px',
-        clickToClose: true,
-        position: 'center-top',
-      });
+      Notify.failure('Oops, there is no country with that name', notifyOptions);
         throw new Error(response.status);
     }
     return response.json();
--- src/index.js
+++ src/index.js
@@ -4,13 +4,54 @@ import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
+const notifyOptions = {
+  timeout: 2500,
+  width: '300px',
+  clickToClose: true,
+  position: 'center-top',
+};
 
 const searchInputEl = document.querySelector('#search-box');
 const countryListEl = document.querySelector('.country-list');
 const countryInfoEl = document.querySelector('.country-info');
 
+const renderContriesList = (countries) => {
+    const markup = countries
+      .map(country => {
+        return `<li>
+          <p>${country.flags.svg}</p>
+          <p>${country.name.official}</p>
+        </li>`;
+      })
+        .join('');
+    countryListEl.innerHTML = markup;
+}
+
+const renderContriesInfo = countries => {
+  const markup = countries
+    .map(country => {
+      return `
+          <p><b>capital</b>: ${country.capital}</p>
+          <p><b>population</b>: ${country.population}</p>
+          <p><b>languages</b>: ${Object.values(country.languages).join(', ')}</p>
+        `;
+    })
+    countryInfoEl.innerHTML = markup;
+};
+
 fetchCountries('cana')
-  .then(countries => console.log(countries))
+    .then(countries => {
+        console.log(countries)
+        console.log(countries.length);
+        if (countries.length > 10) {
+          Notify.info(
+            'Too many matches found. Please enter a more specific name.', notifyOptions
+          );
+          return;
+        }
+        renderContriesList(countries);
+        renderContriesInfo(countries);
+    })
   .catch(error => console.log(error));
 
 const onSearchInput = (e) => {
@@ -19,13 +60,7 @@ const onSearchInput = (e) => {
     console.log(searchCountry.trim().length);
     if (searchCountry.trim().length < 2) {
       Notify.info(
-        'Too many matches found. Please enter a more specific name.',
-        {
-          timeout: 2500,
-          width: '300px',
-          clickToClose: true,
-          position: 'center-top',
-        }
+        'Too many matches found. Please enter a more specific name.', notifyOptions
       );
       return;
     }
