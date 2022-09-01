43159f0761a0669b6014691b851985109eb25521
Kateryna Kleshch <ketrins1993@gmail.com>
2022-09-02 00:03:07 +0300

    add notify and  lodash

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,19 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const fields = ['name', 'capital', 'population', 'flags', 'languages'];
+
 export const fetchCountries = name => {
-  return fetch(`https://restcountries.com/v3.1/name/${name}`).then(response => {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=${fields.join(',')}`
+  ).then(response => {
     if (!response.ok) {
-      throw new Error(response.status);
+      Notify.failure('Oops, there is no country with that name', {
+        timeout: 2500,
+        width: '300px',
+        clickToClose: true,
+        position: 'center-top',
+      });
+        throw new Error(response.status);
     }
     return response.json();
   });
--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
 import './css/styles.css';
-import { fetchCountries } from './fetchCountries.js'
+import { fetchCountries } from './fetchCountries.js';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
 
-
+const searchInputEl = document.querySelector('#search-box');
+const countryListEl = document.querySelector('.country-list');
+const countryInfoEl = document.querySelector('.country-info');
 
 fetchCountries('cana')
   .then(countries => console.log(countries))
   .catch(error => console.log(error));
+
+const onSearchInput = (e) => {
+    const searchCountry = e.target.value;
+    console.log(searchCountry.trim());
+    console.log(searchCountry.trim().length);
+    if (searchCountry.trim().length < 2) {
+      Notify.info(
+        'Too many matches found. Please enter a more specific name.',
+        {
+          timeout: 2500,
+          width: '300px',
+          clickToClose: true,
+          position: 'center-top',
+        }
+      );
+      return;
+    }
+}
+
+searchInputEl.addEventListener('input', debounce(onSearchInput, DEBOUNCE_DELAY))
